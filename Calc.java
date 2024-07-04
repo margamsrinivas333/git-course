c457859b1739e2cb3633860eec36b6490c042210
Srinivas <margamsrinivas333@gmail.com>
2024-07-04 13:54:37 -0400

    Calc class commit

---
+++ Calc.java
@@ -0,0 +1,11 @@
+public class Calc
+{
+    public static void main(String[] args)
+    {
+        int a = 4;
+        int b = 5;
+        int result = a * b;
+
+        System.out.println(result);
+    }
+}
